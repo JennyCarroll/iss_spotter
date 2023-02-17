f5d914bcc6adf6845dddfc2b1228751383976bc7
Jenny Carroll <jennygc@telus.net>
2023-02-17 02:58:26 +0000

    create function getchCoordsByIP that uses request to make an API request and callsback the datat received, which is longitude and latitude, create an if statement that runs if parsing the coordinates into a JSON object fails, resulting in and error message being returned in a callback

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
 // The code below is temporary and can be commented out.
 // const { fetchMyIP } = require("./iss");
 
+// const { fetchCoordsByIP } = require("./iss");
+
+//I don't know if this is correct
+const fetchMyIP = require("./iss").fetchMyIP;
+const fetchCoordsByIP = require("./iss").fetchCoordsByIP;
+
+//we call my fetchMyIP function (that lives in iss.js) and pass in anline function
+// that uses the error and ip information gained by calling fetchMyIP
 fetchMyIP((error, ip) => {
   if (error) {
     console.log("It didn't work!", error);
@@ -10,4 +18,15 @@ fetchMyIP((error, ip) => {
   }
 
   console.log("It worked! Returned IP:", ip);
+
+  // we call my fetchCoordsByIP function (that lives in iss.js) and pass in the IP address and an anline function
+  // that uses the error and data (longitude and lattitude) information gained by calling fetchCoordsByIP
+  fetchCoordsByIP(ip, (error, coordinates) => {
+    if (error) {
+      console.log("It didn't work!", error);
+      return;
+    }
+
+    console.log("It worked! Returned latitude and longitude:", coordinates);
+  });
 });
--- iss.js
+++ iss.js
@@ -5,13 +5,16 @@
 const request = require("request");
 
 // Define a function fetchMyIP which will asynchronously return our IP Address using an API.
-//accepts one parameter, a callback (to pass bak an error or the IP string)
+//getchMyIP is a function that accepts a callback as a parameter and calls it
+// (to pass back an error or the IP string) which it gained through an API request
 const fetchMyIP = function (callback) {
   request("https://api.ipify.org?format=json", (error, response, body) => {
     // error can be set if invalid domain, user is offline, etc.
     //I don't understand why we put null next to error
-    if (error) return callback(error, null);
-
+    if (error) {
+      callback(error, null);
+      return;
+    }
     // if non-200 status, assume server error
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
@@ -24,4 +27,31 @@ const fetchMyIP = function (callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+//Define a function which takes in an IP address and returns the latitude and longitude for it.
+//it should take in two arguments, IP as a string and a callback.
+const fetchCoordsByIP = function (IP, callback) {
+  request(`http://ipwho.is/${IP}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    // In the function, make the request to the API, and have it pass back the relevant
+    // (lat/lng) data as an object via callback.
+    const parsedBody = JSON.parse(body);
+    // check if "success" is true or not
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    }
+    let data = {};
+    data.latitude = parsedBody.latitude;
+    data.longitude = parsedBody.longitude;
+    // The resulting data sent back via the callback should be an object with only two data properties.
+    callback(null, data);
+  });
+};
+
+//is this right?
+module.exports = { fetchMyIP, fetchCoordsByIP };
